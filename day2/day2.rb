92823b7a8dbdaf0dc4eea86332eda8945ab008e0
Jemima Kingsley <jemima@bojates.com>
2023-12-12 17:17:50 +0100

    Solve part 2 in ruby

--- day2/day2.rb
+++ day2/day2.rb
@@ -3,16 +3,7 @@ require_relative '../test_tool'
 MAX_CUBES = { red: 12, green: 13, blue: 14 }
 
 def cubes(filename) 
-  lines = File.readlines(File.join(File.dirname(__FILE__), filename))
-  lines.map! do |line| 
-    game, rounds = line.split(':')
-    game = game.scan(/\d+/).first.to_i
-
-    { game: game, 
-      red: get_max(rounds, 'red'), 
-      green: get_max(rounds, 'green'), 
-      blue: get_max(rounds, 'blue') }
-  end
+  lines = create_hash(filename)
 
   lines.select! do |line|
     line[:red] <= MAX_CUBES[:red] &&
@@ -21,6 +12,25 @@ def cubes(filename)
   end
 
   lines.map { |line| line[:game] }.sum
+end
+
+def get_powers(filename) 
+  lines = create_hash(filename)
+  lines.map { |line| line[:red] * line[:green] * line[:blue]}.sum
+end
+
+def create_hash(filename)
+  lines = File.readlines(File.join(File.dirname(__FILE__), filename))
+
+  lines.map do |line| 
+    game, rounds = line.split(':')
+    game = game.scan(/\d+/).first.to_i
+
+    { game: game, 
+      red: get_max(rounds, 'red'), 
+      green: get_max(rounds, 'green'), 
+      blue: get_max(rounds, 'blue') }
+  end
 end
 
 def get_max(line, search_term)
@@ -32,5 +42,9 @@ def get_max(line, search_term)
   end.max
 end
 
+# Part 1
 test(cubes('testfile1.txt'), 8)
 test(cubes('input.txt'), 2563)
+# Part 2
+test(get_powers('testfile1.txt'), 2286)
+test(get_powers('input.txt'), 70768)
