9af53f554b5102667e945dfb0d82da434a752cfd
Jemima Kingsley <jemima@bojates.com>
2023-12-13 15:02:43 +0100

    Part 2 passing. Horrible code!

--- day3/day3.rb
+++ day3/day3.rb
@@ -66,6 +66,20 @@ For each number, see if there's a symbol either:
   - below and one left or one right of the number
 If there is, add to an array of parts
 Sum the array
+
+## Part 2
+We now only want numbers that share the same * symbol. 
+We then multiply them by each other, and return a summed amount. 
+
+Could: 
+ - only keep numbers that have a *
+ - Use the location of * as a key, and hold the digits in an array [line_char]
+ - get rid of arrays with only one entry
+ - check arrays with more than 2 entries
+ - work with the rest
+
+OR
+ - if we find a star, look for digits?
 =end
 require_relative '../test_tool'
 
@@ -127,6 +141,67 @@ def check_for_symbol(lines, line_idx, char_idx)
   end
 end
 
-test(sum_parts("testfile1.txt"), 4361)
-test(sum_parts("testfile2.txt"), 1752)
-test(sum_parts("input.txt"), 533775) 
+def gear_ratios(filename)
+  lines = get_lines(filename)
+
+  parts = {}
+  lines.each_with_index do |line, line_idx|
+    # next if line_idx > 1
+    digit_str = ''
+    locations = []
+    line.chars.each_with_index do |char, idx|
+      if /\d/=~char
+        digit_str += char
+        locations += check_for_stars(lines, line_idx, idx)
+        # p locations
+        # if check_for_stars(lines, line_idx, idx)
+        #   locations << 
+        # end
+      else 
+        locations.each do |l|
+          parts[l] = [digit_str.to_i, parts[l]].flatten.compact.uniq
+        end
+        digit_str = ''
+        locations = []
+      end
+      
+      if (idx == line.length - 1) && locations.length > 0
+        locations.each do |l|
+        parts[l] = [digit_str.to_i, parts[l]].flatten.compact.uniq
+        end
+      end
+    end
+  end
+  parts.select { |key, val| val.length == 2 }.map { |_, (val1, val2)| val1 * val2 }.sum
+
+  # parts.map(&:to_i).sum
+end
+
+def check_for_stars(lines, line_idx, char_idx)
+  re = /\*/
+
+  first_char = (char_idx == 0)
+  last_char = (lines.length - 1 == char_idx)
+  first_line = (line_idx == 0)
+  last_line = !(lines[line_idx + 1])
+
+  hits = []
+  hits << ("#{line_idx}_#{char_idx - 1}" if re =~ lines[line_idx][char_idx - 1]) unless first_char
+  hits << ("#{line_idx}_#{char_idx + 1}" if re =~ lines[line_idx][char_idx + 1]) unless last_char
+  hits << ("#{line_idx - 1}_#{char_idx}" if re =~ lines[line_idx - 1][char_idx]) unless first_line
+  hits << ("#{line_idx - 1}_#{char_idx - 1}" if re =~ lines[line_idx - 1][char_idx - 1]) unless first_line || first_char
+  hits << ("#{line_idx - 1}_#{char_idx + 1}" if re =~ lines[line_idx - 1][char_idx + 1]) unless first_line || last_char
+  hits << ("#{line_idx + 1}_#{char_idx}" if re =~ lines[line_idx + 1][char_idx]) unless last_line
+  hits << ("#{line_idx + 1}_#{char_idx - 1}" if re =~ lines[line_idx + 1][char_idx - 1]) unless last_line || first_char
+  hits << ("#{line_idx + 1}_#{char_idx + 1}" if re =~ lines[line_idx + 1][char_idx + 1]) unless last_line || last_char
+
+  # p hits.compact
+  return hits.compact
+end
+
+test(gear_ratios("testfile1.txt"), 467835)
+test(gear_ratios("input.txt"), 78236071)
+
+# test(sum_parts("testfile1.txt"), 4361)
+# test(sum_parts("testfile2.txt"), 1752)
+# test(sum_parts("input.txt"), 533775) 
