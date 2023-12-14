593c3e173670c4740e4a4403acfdb50219269a8e
Jemima Kingsley <jemima@bojates.com>
2023-12-14 13:41:13 +0100

    Partial refactor to use Regexp.last_match

--- day3/day3.rb
+++ day3/day3.rb
@@ -80,65 +80,41 @@ Could:
 
 OR
  - if we find a star, look for digits?
+
+ Refactored the first bit to use strings and Regexp.last_match with the offset info. 
+
+ # Get the digits and the location of the digits
+ # For each digit, we have line and range
+ # We can check char before and after on same line
+ # We can check range on line above
+ # We can check range on live below. 
+ # If we have a regexp match, keep the digits
 =end
 require_relative '../test_tool'
 
+
 def sum_parts(filename)
   lines = get_lines(filename)
-
-  parts = []
-  lines.each_with_index do |line, line_idx|
-    # next if line_idx > 1
-    digit_str = ''
-    keep_me = false
-
-    line.chars.each_with_index do |char, idx|
-      if /\d/=~char
-        digit_str += char
-        if !keep_me && check_for_symbol(lines, line_idx, idx)
-          keep_me = true
-        end
-      else 
-        if keep_me
-          parts << digit_str
-        end
-        digit_str = ''
-        keep_me = false
-      end
-
-      if (idx == line.length - 1) && keep_me
-        parts << digit_str
-      end
+  lines_select = lines.map.with_index do |line, i| 
+    line.gsub(/\d+/).map do |num|
+      
+      a, b = Regexp.last_match.offset(0)
+      check_me(lines, i, a, b) ? num : nil
     end
-  end
-  parts.map(&:to_i).sum
+  end.flatten.map(&:to_i).sum
 end
 
-def get_lines(filename)
-  File.readlines(File.join(File.dirname(__FILE__), filename), chomp: true)
-end
+def check_me(lines, line, x, y)
+  regexp = /[^0-9\.]/
 
-def check_for_symbol(lines, line_idx, char_idx)
-  re = /([^a-zA-Z0-9\.])/
-
-  first_char = (char_idx == 0)
-  last_char = (lines.length - 1 == char_idx)
-  first_line = (line_idx == 0)
-  last_line = !(lines[line_idx + 1])
-
-  chars = []
-  chars << lines[line_idx][char_idx - 1] unless first_char
-  chars << lines[line_idx][char_idx + 1] unless last_char
-  chars << lines[line_idx - 1][char_idx] unless first_line
-  chars << lines[line_idx - 1][char_idx - 1] unless first_line || first_char
-  chars << lines[line_idx - 1][char_idx + 1] unless first_line || last_char
-  chars << lines[line_idx + 1][char_idx] unless last_line
-  chars << lines[line_idx + 1][char_idx - 1] unless last_line || first_char
-  chars << lines[line_idx + 1][char_idx + 1] unless last_line || last_char
-
-  if re =~ chars.join
-    return true
-  end
+  x = x - 1 unless x == 0
+  y = y + 1 unless y == lines[line].length - 1 
+  line_above = (line == 0) ? line : line - 1
+  line_below = (line == lines.length - 1) ? line : line + 1
+  
+  lines[line][x...y] =~ regexp ||
+    lines[line_above][x...y] =~ regexp ||
+    lines[line_below][x...y] =~ regexp 
 end
 
 def gear_ratios(filename)
@@ -153,10 +129,6 @@ def gear_ratios(filename)
       if /\d/=~char
         digit_str += char
         locations += check_for_stars(lines, line_idx, idx)
-        # p locations
-        # if check_for_stars(lines, line_idx, idx)
-        #   locations << 
-        # end
       else 
         locations.each do |l|
           parts[l] = [digit_str.to_i, parts[l]].flatten.compact.uniq
@@ -172,9 +144,10 @@ def gear_ratios(filename)
       end
     end
   end
-  parts.select { |key, val| val.length == 2 }.map { |_, (val1, val2)| val1 * val2 }.sum
 
-  # parts.map(&:to_i).sum
+  parts.select { |key, val| val.length == 2 }
+        .map { |_, (val1, val2)| val1 * val2 }
+        .sum
 end
 
 def check_for_stars(lines, line_idx, char_idx)
@@ -195,13 +168,17 @@ def check_for_stars(lines, line_idx, char_idx)
   hits << ("#{line_idx + 1}_#{char_idx - 1}" if re =~ lines[line_idx + 1][char_idx - 1]) unless last_line || first_char
   hits << ("#{line_idx + 1}_#{char_idx + 1}" if re =~ lines[line_idx + 1][char_idx + 1]) unless last_line || last_char
 
-  # p hits.compact
   return hits.compact
+end
+
+def get_lines(filename)
+  filename = File.join(File.dirname(__FILE__), filename)
+  File.readlines(filename, chomp: true)
 end
 
 test(gear_ratios("testfile1.txt"), 467835)
 test(gear_ratios("input.txt"), 78236071)
 
-# test(sum_parts("testfile1.txt"), 4361)
-# test(sum_parts("testfile2.txt"), 1752)
-# test(sum_parts("input.txt"), 533775) 
+test(sum_parts("testfile1.txt"), 4361)
+test(sum_parts("testfile2.txt"), 1752)
+test(sum_parts("input.txt"), 533775) 
