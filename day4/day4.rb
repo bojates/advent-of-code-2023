49c85b8b95f541e7719c310b862f019a71c6cfbc
Jemima Kingsley <jemima@bojates.com>
2023-12-14 14:50:19 +0100

    Ruby passing second part

--- day4/day4.rb
+++ day4/day4.rb
@@ -17,5 +17,28 @@ def get_score(filename)
   end.sum
 end
 
-test(get_score('input.txt'), 22193) # 24896 is too high
+def get_number_scratchcards(filename)
+  lines = get_lines(filename)
+
+  counter = Array.new(lines.size, 1)
+  
+  lines.each_with_index do |line, i|
+    _, winners, mine = line.split(/[:|]/).map do |chunk|
+      chunk.scan(/\d+/).map(&:to_i)
+    end
+    
+    win_count = winners.intersection(mine).size
+    
+    (i + 1.. i + win_count).each do |j|
+      counter[j] += (1 * counter[i])
+    end
+  end
+  
+  counter.sum
+end
+
+test(get_score('input.txt'), 22193) 
 test(get_score('testfile1.txt'), 13)
+
+test(get_number_scratchcards('testfile1.txt'), 30)
+test(get_number_scratchcards('input.txt'), 5625994)
