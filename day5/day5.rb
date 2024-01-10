ff4163281464d33dcf5de5ad7ed3f23f6bdf3335
Jemima Kingsley <jemima@bojates.com>
2024-01-10 17:52:49 +0000

    simplify

--- day5/day5.rb
+++ day5/day5.rb
@@ -9,36 +9,27 @@ def get_closest_location(filename)
   lines = get_lines(filename)
 
   seeds = lines[0].split(" ").select { |section| section.match?(/\d+/) }.map(&:to_i)
+
   mappings = {}
   current_key = ""
   lines.each do |line|
     if /map:/.match?(line)
       current_key = line
-      mappings[current_key] = []
+      mappings[current_key] = {}
     elsif /^\d+\s\d+\s\d+$/.match?(line)
       dest, source, length = line.split(" ").map(&:to_i)
       line_range = (source..source + length)
-      mappings[current_key] << {line_range => dest - source}
+      mappings[current_key].merge!({line_range => dest - source})
     end
   end
 
   locations = []
-  map_keys = [
-    "seed-to-soil map:",
-    "soil-to-fertilizer map:",
-    "fertilizer-to-water map:",
-    "water-to-light map:",
-    "light-to-temperature map:",
-    "temperature-to-humidity map:",
-    "humidity-to-location map:"
-  ]
-
   seeds.each do |seed|
     find_loc = seed
 
-    map_keys.each do |map_key|
-      find_loc = mappings[map_key].map { |rng|
-        rng.keys.first.cover?(find_loc) ? (find_loc + rng.values.first) : nil
+    mappings.each do |_, values|
+      find_loc = values.map { |rng, val|
+        rng.cover?(find_loc) ? (find_loc + val) : nil
       }.compact.first || find_loc
     end
 
